e959d7811bde2569df5f7d88df165065fcf58d8d
richard.lee <richard.lee@ct-1-demacbook.local>
2025-05-14 15:16:50 +0800

    Set devindicator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  devIndicators: process.env.NODE_ENV !== "production" ? undefined : false,
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
