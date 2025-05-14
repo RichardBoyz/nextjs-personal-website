06ebcbf3ce3ec0e0aa74829e3b211392713d040c
richard.lee <richard.lee@ct-1-demacbook.local>
2025-05-14 17:09:20 +0800

    Reset home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,18 +1,20 @@
+"use client";
 import { Locale, useTranslations } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 import { use } from "react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 type Props = {
   params: Promise<{ locale: Locale }>;
 };
 
 export default function IndexPage({ params }: Props) {
-  const { locale } = use(params);
-
-  // Enable static rendering
-  setRequestLocale(locale);
-
   const t = useTranslations("home");
 
-  return <p className="max-w-[590px]">{t("title")}</p>;
+  return (
+    <>
+      <GoogleAnalytics />
+      <p className="max-w-[590px]">{t("title")}</p>
+    </>
+  );
 }
