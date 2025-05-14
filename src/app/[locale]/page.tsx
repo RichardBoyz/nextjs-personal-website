5b51a4047632626044a9b2130deb6b4e88046e53
richard.lee <richard.lee@ct-1-demacbook.local>
2025-05-14 11:09:02 +0800

    Set i18n

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,18 @@
+import { Locale, useTranslations } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
+import { use } from "react";
+
+type Props = {
+  params: Promise<{ locale: Locale }>;
+};
+
+export default function IndexPage({ params }: Props) {
+  const { locale } = use(params);
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
+  const t = useTranslations("home");
+
+  return <p className="max-w-[590px]">{t("title")}</p>;
+}
